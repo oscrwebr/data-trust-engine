7c966e3094b22b18b913def83e9947942bce2f05
Szymon Wodkiewicz <wodkiewiczs@cardiff.ac.uk>
2026-02-18 18:32:53 +0000

    Initialised alembic to be used as our db migration tool

--- backend/app/scanning/models.py
+++ backend/app/scanning/models.py
@@ -1,9 +1,9 @@
 from sqlalchemy import Boolean, Column, Integer, String
-from core.database import Base
+from app.core.database import Base
 
 class File(Base):
     __tablename__ = 'file'
 
     file_id = Column(Integer, primary_key=True, index=True) 
-    file_extension = Column(String)
-    hash = Column(String)
+    file_extension = Column(String(16))
+    hash = Column(String(64))
--- backend/app/scanning/router.py
+++ backend/app/scanning/router.py
@@ -5,8 +5,8 @@ from .service import hash_file
 router = APIRouter(prefix="/scanning", tags=["scanning"])
 
 @router.get("/hash")
-def run_hash_endpoint(file_path):
-    hash_result = hash_file(file_path)
+def run_hash_endpoint(file_id):
+    hash_result = hash_file(file_id)
     print(hash_result)
 
     return
