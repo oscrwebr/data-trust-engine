e65d355a804ed127156204f0b760707228a6f709
Oscar Webster <WebsterO1@cardiff.ac.uk>
2026-03-11 11:08:55 +0000

    Added navbar items (logo, pages) and styling

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,10 +1,24 @@
 import "./navbar.css";
+import logo from "../../assets/CIH_long_logo.png";
+import { Link } from "react-router-dom";
+
 
 function Navbar() {
 
   return (
     <div className="navbar-base">
-        <h1>Navbar</h1>
+        {/* "/" placeholder for whatever page ends up being home/main page */}
+        <Link to="/">
+            <div className="navbar-logo">
+                <img src={logo} alt="CIH Logo"/>
+            </div>
+        </Link>
+        <div className="navbar-list">
+            <ul className="navbar-items">
+                <li>Files</li>
+                <li>Scans</li>
+            </ul>
+        </div>
     </div>
   );
 }
