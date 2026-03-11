631933a054fe465e39ded370b1a1d6066044040e
Oscar Webster <WebsterO1@cardiff.ac.uk>
2026-03-11 16:02:04 +0000

    made it so navbar isn't displayed on home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css"
 import { useRef } from "react";
 
-import { BrowserRouter, Routes, Route, useNavigate} from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate, useLocation} from "react-router-dom";
 import Dashboard from './dashboard/Dashboard';
 import EmployeeInviteError from './invites/error.jsx';
 import Roles from "./roles/roles";
@@ -32,11 +32,18 @@ function Home() {
 function App() {
   const toast = useRef(null);
 
+  // Not making navbar visible on 'home' page
+  // Ref: https://www.reddit.com/r/reactjs/comments/kvoj5d/comment/gtfmh2s/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+  const location = useLocation();
+
   return (
-    <BrowserRouter>
-      <Toast ref={toast} position="top-right"/>
-    <Navbar/>
-    <div className="content-frame">
+    <>
+    <Toast ref={toast} position="top-right"/>
+
+    
+      
+    {location.pathname !== "/" && <Navbar />}
+    <div className={location.pathname !== "/" ? "content-frame" : ""}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/roles" element={<Roles />} />
@@ -48,7 +55,7 @@ function App() {
         <Route path="/invite-error/:type" element={<EmployeeInviteError />} />
       </Routes>
     </div>
-    </BrowserRouter>
+    </>
   );
 }
 
