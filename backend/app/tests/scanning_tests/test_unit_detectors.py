61a85beb79488cb2dab16510a9c3eb902f393cf9
Szymon Wodkiewicz <wodkiewiczs@cardiff.ac.uk>
2026-03-12 18:15:10 +0000

    implemented unit tests for legal case information detectors

--- backend/app/tests/scanning_tests/test_unit_detectors.py
+++ backend/app/tests/scanning_tests/test_unit_detectors.py
@@ -111,3 +111,67 @@ def test_detects_vats():
     assert detections[0]["page_number"] == 1
     assert detections[1]["sensitivity_subcategory"] == "VAT"
     assert detections[1]["page_number"] == 2
+
+
+def test_detects_citations():
+    text_dict = {
+        1: "This is a valid citation [2026] AC 690",
+        2: "This string should not be detected",
+        3: "This citation [2016] EWCA Civ 395 should also be detected",
+    }
+
+    detections = detect_citations(text_dict=text_dict)
+
+    assert len(detections) == 2
+    assert detections[0]["sensitivity_subcategory"] == "CITATION"
+    assert detections[0]["page_number"] == 1
+    assert detections[1]["sensitivity_subcategory"] == "CITATION"
+    assert detections[1]["page_number"] == 3
+
+
+def test_detect_acts():
+    text_dict = {
+        1: "This is not an act detection",
+        2: "The act Benefits Act 1992 was used in this string",
+        3: "Welfare Reform Act 2004 should be detected"
+    }
+
+    detections = detect_acts(text_dict=text_dict)
+
+    assert len(detections) == 2
+    assert detections[0]["sensitivity_subcategory"] == "ACT"
+    assert detections[0]["page_number"] == 2
+    assert detections[1]["sensitivity_subcategory"] == "ACT"
+    assert detections[1]["page_number"] == 3
+
+
+def test_detect_regulations():
+    text_dict = {
+        1: "This is not a regulation detection",
+        2: "The document refers to Regulation (EU) 2016/679.",
+        3: "Another valid one is Regulation (UK) 2024/1021 in this text",
+    }
+
+    detections = detect_regulations(text_dict=text_dict)
+
+    assert len(detections) == 2
+    assert detections[0]["sensitivity_subcategory"] == "REGULATION"
+    assert detections[0]["page_number"] == 2
+    assert detections[1]["sensitivity_subcategory"] == "REGULATION"
+    assert detections[1]["page_number"] == 3
+
+
+def test_detect_case_names():
+    text_dict = {
+        1: "This case is regarding the case of Vodaphone v Sky",
+        2: "Smech Properties Ltd v Runnymede Borough Council is another valid detection",
+        3: "There is no case name to be detected in this string"
+    }
+
+    detections = detect_case_names(text_dict=text_dict)
+
+    assert len(detections) == 2
+    assert detections[0]["sensitivity_subcategory"] == "CASE_NAME"
+    assert detections[0]["page_number"] == 1
+    assert detections[1]["sensitivity_subcategory"] == "CASE_NAME"
+    assert detections[1]["page_number"] == 2
