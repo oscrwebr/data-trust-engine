df3f4a21a0d251f13adba5b0734613d71d75f4a5
Oscar Webster <WebsterO1@cardiff.ac.uk>
2026-03-13 17:11:21 +0000

    Added MUI Joy table component and DateTime formatter function

--- frontend/src/scans/Scans.jsx
+++ frontend/src/scans/Scans.jsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from "react";
 import api from "../api/axiosConfig";
+// import { DataTable } from 'primereact/datatable';
+// import { Column } from 'primereact/column';
+import Table from '@mui/joy/Table';
+import Sheet from '@mui/joy/Sheet';
+
 
 function Scans(){
 
@@ -20,15 +25,42 @@ function Scans(){
 
     }, []);
 
+    const formatDateTime = (dateTimeString) => {
+        const date = new Date(dateTimeString);
+        const formattedDate = date.toLocaleDateString();
+        const formattedTime = date.toLocaleTimeString();
+        return `${formattedDate} ${formattedTime}`;
+    }
+
 
     return (
         <div>
             <h1>Scans</h1>
-                <ul>
-                    {scans.map(scan => (
-                        <li key={scan.scan_id}>Scan: {scan.scan_id}</li>
-                    ))}
-                </ul>
+            <Sheet>
+                {/* Table component from MUI Joy: https://mui.com/joy-ui/react-table/ */}
+                <Table stickyFooter={false} stickyHeader stripe="even" variant="plain" aria-label="Scans table" hoverRow>
+                    <thead>
+                        <tr>
+                            <th>Scan ID</th>
+                            <th>Started At</th>
+                            <th>Finished At</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {scans.map((scan) => (
+                            <tr key={scan.scan_id}>
+                                <td>{scan.scan_id}</td>
+                                <td>{formatDateTime(scan.started_at)}</td>
+                                <td>{formatDateTime(scan.finished_at)}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+
+
+                </Table>
+                
+
+            </Sheet>
         </div>
     );
 }
