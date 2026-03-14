07ecdc1af6dce556da869605dd63403c4e7f1b8d
Oscar Webster <WebsterO1@cardiff.ac.uk>
2026-03-14 17:20:48 +0000

    Added top section of ScanCard displaying scan type and status

--- frontend/src/scans/ScanCard.jsx
+++ frontend/src/scans/ScanCard.jsx
@@ -1,5 +1,8 @@
 import "./scans.css";
 import { PiFileMagnifyingGlass, PiFolders } from "react-icons/pi";
+import { PiCheckCircle } from "react-icons/pi";
+import { PiClockClockwise } from "react-icons/pi";
+
 
 
 function ScanCard({ scan }) {
@@ -23,9 +26,34 @@ function ScanCard({ scan }) {
                             ? <PiFolders/>
                             : <PiFileMagnifyingGlass/>
 
+    const scanTypeText = scan.scan_type === "organisation"
+                            ? "Organisational"
+                            : "Sensitivity"
+
+    const scanStatusIcon = scan.finished_at 
+                            ? <PiCheckCircle/>
+                            : <PiClockClockwise/>
+
     return(
 
         <div className={`scan-card ${scanTypeClassName}`}>
+            <div className="scan-card-top">
+                <div className="scan-card-top-left">
+                    <span className={`scan-type-icon scan-type-icon-${scan.scan_type}`}>
+                        {scanTypeIcon}
+                    </span>
+
+                    <span className={`scan-type-text scan-type-text-${scan.scan_type}`}>
+                        {scanTypeText}
+                    </span>
+                </div>
+                <div className="scan-card-top-right">
+                    <span className={`scan-status-text ${scan.finished_at ? "scan-status-completed" : "scan-status-ongoing"}`}>
+                        {scanStatusIcon}
+                        {scan.finished_at ? "Completed" : "Ongoing"}
+                    </span>
+                </div>
+            </div>
             <span>{scan.scan_id}</span>
             <span>{formatDateTime(scan.started_at)}</span>
             <span>{formatDateTime(scan.finished_at)}</span>
