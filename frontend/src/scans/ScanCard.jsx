08d8c0d02b4d3ba0296b2b513b5400b34ed1b244
Oscar Webster <WebsterO1@cardiff.ac.uk>
2026-03-14 15:36:00 +0000

    Added conditionals to display different types of ScanCards depending on the scan_type

--- frontend/src/scans/ScanCard.jsx
+++ frontend/src/scans/ScanCard.jsx
@@ -1,7 +1,11 @@
 import "./scans.css";
+import { PiFileMagnifyingGlass, PiFolders } from "react-icons/pi";
+
 
 function ScanCard({ scan }) {
 
+
+    // Format date into dd/mm/yyyy, hh:mm:ss
         const formatDateTime = (dateTimeString) => {
         const date = new Date(dateTimeString);
         const formattedDate = date.toLocaleDateString();
@@ -9,10 +13,22 @@ function ScanCard({ scan }) {
         return `${formattedDate}, ${formattedTime}`;
     }
 
+    // Setting the ScanCard visuals for each scan type
+    // Only organisation and sensitivity type scans for now, can be adjusted to include other types if needed
+    const scanTypeClassName = scan.scan_type === "organisation" 
+                            ? "scan-type-organisation"
+                            : "scan-type-sensitivity";
+
+    const scanTypeIcon = scan.scan_type === "organisation"
+                            ? <PiFolders/>
+                            : <PiFileMagnifyingGlass/>
+
     return(
 
-        <div className="scan-card">
+        <div className={`scan-card ${scanTypeClassName}`}>
             <span>{scan.scan_id}</span>
+            <span>{formatDateTime(scan.started_at)}</span>
+            <span>{formatDateTime(scan.finished_at)}</span>
         </div>
     )
 }
