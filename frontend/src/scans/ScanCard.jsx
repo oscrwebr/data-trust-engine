879a51236780e0fcf97f56a2a5fd0a8942a5601e
Oscar Webster <WebsterO1@cardiff.ac.uk>
2026-03-15 03:54:38 +0000

    Turned each ScanCard into a Link component that navigates to /scans/{scan_id}

--- frontend/src/scans/ScanCard.jsx
+++ frontend/src/scans/ScanCard.jsx
@@ -4,6 +4,7 @@ import { PiCheckCircle } from "react-icons/pi";
 import { PiClockClockwise } from "react-icons/pi";
 import { Divider } from 'primereact/divider';
 import { PiWarningCircle } from "react-icons/pi";
+import { Link } from "react-router-dom";
 
 
 
@@ -45,70 +46,72 @@ function ScanCard({ scan }) {
                             : <PiClockClockwise/>
 
     return(
-        <div className={`scan-card ${scanTypeClassName}`}>
-            {/* Top section of the ScanCard split into two sections */}
-            <div className="scan-card-top">
-                {/* Top left displays type of scan */}
-                <div className="scan-card-top-left">
-                    {/* Scan type icon */}
-                    <span className={`scan-type-icon scan-type-icon-${scan.scan_type}`}>
-                        {scanTypeIcon}
-                    </span>
-                    {/* Scan type text */}
-                    <span className={`scan-type-text scan-type-text-${scan.scan_type}`}>
-                        {scanTypeText}
-                    </span>
+        <Link to={`/scans/${scan.scan_id}`}>
+            <div className={`scan-card ${scanTypeClassName}`}>
+                {/* Top section of the ScanCard split into two sections */}
+                <div className="scan-card-top">
+                    {/* Top left displays type of scan */}
+                    <div className="scan-card-top-left">
+                        {/* Scan type icon */}
+                        <span className={`scan-type-icon scan-type-icon-${scan.scan_type}`}>
+                            {scanTypeIcon}
+                        </span>
+                        {/* Scan type text */}
+                        <span className={`scan-type-text scan-type-text-${scan.scan_type}`}>
+                            {scanTypeText}
+                        </span>
+                    </div>
+                    {/* Top right displays the scan status */}
+                    <div className="scan-card-top-right">
+                        {/* Scan status can be either completed or ongoing (could modify to include failed in future) */}
+                        <span className={`scan-status-text ${scan.finished_at ? "scan-status-completed" : "scan-status-ongoing"}`}>
+                            {scanStatusIcon}
+                            {scan.finished_at ? "Completed" : "Ongoing"}
+                        </span>
+                    </div>
                 </div>
-                {/* Top right displays the scan status */}
-                <div className="scan-card-top-right">
-                    {/* Scan status can be either completed or ongoing (could modify to include failed in future) */}
-                    <span className={`scan-status-text ${scan.finished_at ? "scan-status-completed" : "scan-status-ongoing"}`}>
-                        {scanStatusIcon}
-                        {scan.finished_at ? "Completed" : "Ongoing"}
-                    </span>
+                {/* Scan ID section */}
+                <div className="scan-card-id-section">
+                    <span className="scan-card-id-heading">Scan ID</span>
+                    <span className="scan-card-id">{scan.scan_id}</span>
+                </div>
+                {/* Scan Started At and Finished At section */}
+                <div className="scan-card-dates">
+                    {/* Started At block */}
+                    <div className="scan-card-date-block">
+                        <span className="scan-card-id-heading">Started At</span>
+                        <span className="scan-card-value">
+                            {formatDateTime(scan.started_at)}
+                        </span>
+                    </div>
+                    {/* Finished At block */}
+                    <div className="scan-card-date-block">
+                        <span className="scan-card-id-heading">Finished At</span>
+                        <span className="scan-card-value">
+                            {formatDateTime(scan.finished_at)}
+                        </span>
+                    </div>
+                </div>
+                {/* From PrimeReact: https://primereact.org/divider/ */}
+                <Divider className="scan-card-divider"/>
+                <div className="scan-card-dates">
+                    <div className="scan-card-date-block">
+                        <span className="scan-card-id-heading">Total Files</span>
+                        <span className="scan-card-value scan-card-total-files">
+                            <span>{scan.file_count}</span>
+                        </span>
+                    </div>
+                    <div className="scan-card-date-block">
+                        <span className="scan-card-id-heading">Files with Issues</span>
+                        <span className="scan-card-value scan-card-issue-files">
+                            {/* Hardcoded for now */}
+                            <span>2</span>
+                            <PiWarningCircle/>
+                        </span>
+                    </div>
                 </div>
             </div>
-            {/* Scan ID section */}
-            <div className="scan-card-id-section">
-                <span className="scan-card-id-heading">Scan ID</span>
-                <span className="scan-card-id">{scan.scan_id}</span>
-            </div>
-            {/* Scan Started At and Finished At section */}
-            <div className="scan-card-dates">
-                {/* Started At block */}
-                <div className="scan-card-date-block">
-                    <span className="scan-card-id-heading">Started At</span>
-                    <span className="scan-card-value">
-                        {formatDateTime(scan.started_at)}
-                    </span>
-                </div>
-                {/* Finished At block */}
-                <div className="scan-card-date-block">
-                    <span className="scan-card-id-heading">Finished At</span>
-                    <span className="scan-card-value">
-                        {formatDateTime(scan.finished_at)}
-                    </span>
-                </div>
-            </div>
-            {/* From PrimeReact: https://primereact.org/divider/ */}
-            <Divider className="scan-card-divider"/>
-            <div className="scan-card-dates">
-                <div className="scan-card-date-block">
-                    <span className="scan-card-id-heading">Total Files</span>
-                    <span className="scan-card-value scan-card-total-files">
-                        <span>{scan.file_count}</span>
-                    </span>
-                </div>
-                <div className="scan-card-date-block">
-                    <span className="scan-card-id-heading">Files with Issues</span>
-                    <span className="scan-card-value scan-card-issue-files">
-                        {/* Hardcoded for now */}
-                        <span>2</span>
-                        <PiWarningCircle/>
-                    </span>
-                </div>
-            </div>
-        </div>
+        </Link>
     )
 }
 
