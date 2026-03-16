cd1f809c471499001856fc4f882f112622b50242
Daiyan Khan <khand6@cardiff.ac.uk>
2026-03-16 13:05:41 +0000

    Have created the user_files and user_folders tables

--- backend/app/ingestion/models.py
+++ backend/app/ingestion/models.py
@@ -1,12 +1,13 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey, UniqueConstraint
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey, UniqueConstraint, Index
 from ..core.database import Base
+from ..authentication.models import User
 
 class Folder(Base):
     __tablename__ = 'folder'
 
-    folder_id = Column(Integer(), primary_key=True, index=True)
+    folder_id = Column(Integer(), primary_key=True)
     graph_id = Column(String(100), nullable=True, index=True)
-    UniqueConstraint(graph_id)
+    UniqueConstraint(graph_id, name="uq_graph_id")
     name = Column(String(255), nullable=False)
     web_url = Column(Text())
     parent_graph_id = Column(String(100), ForeignKey("folder.graph_id"), index=True)
@@ -15,7 +16,7 @@ class Folder(Base):
 class IngestionFile(Base):
     __tablename__ = 'ingestion_file'
 
-    ingestion_file_id = Column(Integer(), primary_key=True, index=True)
+    ingestion_file_id = Column(Integer(), primary_key=True)
     graph_id = Column(String(100), nullable=False, index=True)
     name = Column(String(255), nullable=False)
     extension = Column(String(255), nullable=False)
@@ -26,4 +27,16 @@ class IngestionFile(Base):
     web_url = Column(Text(), nullable=False)
     parent_graph_id = Column(ForeignKey(Folder.graph_id), index=True)
 
+class UserFolders(Base):
+    __tablename__ = 'user_folders'
 
+    folder_id = Column(Integer(), primary_key=True)
+    user_id = Column(ForeignKey(User.user_id), primary_key=True)
+    Index("rev_idx_user_folders", user_id, folder_id)
+
+class UserFiles(Base):
+    __tablename__ = 'user_files'
+
+    file_id = Column(ForeignKey(IngestionFile.ingestion_file_id), primary_key=True)
+    user_id = Column(ForeignKey(User.user_id), primary_key=True)
+    Index("rev_idx_user_files", user_id, file_id)
