c1758f0b466f6f3e957f7695faccd3e916b20daa
Daiyan Khan <khand6@cardiff.ac.uk>
2026-03-26 16:11:35 +0000

    Added first integration test for authentication that ensures refresh token and driveId are added to the database when a user signs up

--- backend/app/tests/authentication_tests/test_signup.py
+++ backend/app/tests/authentication_tests/test_signup.py
@@ -1,13 +1,40 @@
-from app.authentication.service import create_user
-from app.workspaces.models import Notification
-from app.workspaces.repository import add_workspace
-from app.invites.repository import add_invite
-from sqlalchemy import insert
-from app.authentication.models import User
+from app.main import app
+from fastapi import APIRouter, Request
+from sqlalchemy import insert, select
 from PIL import Image
 from io import BytesIO
 from datetime import datetime
 import secrets
+import requests
+import pytest
+
+from app.authentication.service import create_user, DRIVE_DATA_GRAPH_URL
+from app.workspaces.models import Notification
+from app.workspaces.repository import add_workspace
+from app.invites.repository import add_invite
+from app.authentication.models import User
+from app.authentication.router import get_session
+from app.core.security import application, decrypt_refresh
+from app.core.database import get_database
+
+# Mocking the MSAL dependency
+class FakeMsal():
+    def acquire_token_by_auth_code_flow(self, *args, **kwargs): # We don't care about what is actually passed in - just that this is returned!
+        return {
+            "id_token_claims": {
+                "oid": "000000-7sdf77-88asdf8-9sdiy99",
+                "name": "John Katherine Smith",
+                "email": "jkatherinesmith@outlook.com",
+                "preferred_username": "jkatherinesmith@outlook.com"
+            },
+            "refresh_token": "refresh_from_dummy_MSAL_class",
+            "access_token": "access_from_dummy_MSAL_class"
+        }
+# Mocking the get_session function that's used for dependency injection
+class FakeRequests():
+    def __init__(self):
+        self.session = {}
+
 
 # Creating test image 
 def create_test_image():
@@ -84,3 +111,48 @@ def test_signup_fails_with_bad_role(client):
     response = client.send(request = req)
     
     assert response.status_code == 400
+
+# Integration test that ensures a user's refresh token and driveId are added when they sign up'
+def test_drive_id_and_refresh_added_for_user(client, db, requests_mock):
+    # Setting up FakeRequests class for dependency injection override
+    fake_requests = FakeRequests()
+    fake_requests.session["flow"] = "flow"
+    fake_requests.session["next"] = "/test"
+    fake_requests.session["signup"] = True
+    fake_requests.session["role"] = 1
+    fake_requests.session["workspace_id"] = 1
+
+    # overriding dependencies used by the router for '/success/'
+    app.dependency_overrides[application] = lambda: FakeMsal()
+    app.dependency_overrides[get_database] = lambda: db
+    app.dependency_overrides[get_session] = lambda: fake_requests
+
+    # Setting the URL interception for getting the driveId
+    mock_get = requests_mock.get(
+        DRIVE_DATA_GRAPH_URL,
+        json={
+            "id": "test_drive_id"
+        },
+        status_code=200
+        )
+    
+    # Ensuring that there is no user in the user table
+    select_statement = select(User)
+    users = db.execute(select_statement).all()
+    assert users == []
+
+    # GET request for the '/success/' router
+    client.get("/auth/success/")
+
+    # Selecting the user from the db and ensuring that all the details required to be added are there
+    users = db.execute(select_statement).all()[0][0]
+    assert users.firstname == "John"
+    assert users.surname == "Smith"
+    assert users.email == "jkatherinesmith@outlook.com"
+    assert users.username == "jkatherinesmith@outlook.com"
+    assert users.oid == "000000-7sdf77-88asdf8-9sdiy99"
+    assert decrypt_refresh(users.refresh) == "refresh_from_dummy_MSAL_class"
+    assert users.driveId == "test_drive_id"
+
+    # ensure that the request for driveId was made with the access token
+    assert mock_get.request_history[0]._request.headers.get("Authorization") == "Bearer access_from_dummy_MSAL_class"
