3cb8e8152e2e942b824611a1bdf468944f8e2a13
Szymon Wodkiewicz <wodkiewiczs@cardiff.ac.uk>
2026-03-26 14:10:47 +0000

    created styling for file scan history items

--- frontend/src/FileOverview/FileScanHistoryItem.jsx
+++ frontend/src/FileOverview/FileScanHistoryItem.jsx
@@ -1,13 +1,22 @@
+import styles from "./FileOverview.module.css";
+
 function FileScanHistoryItem({ scan }) {
+    const formatted_finish_date = new Date(scan.finished_at).toLocaleString();
+    const is_completed = !!scan.finished_at;
+
     return (
-        <div className="history-item">
-            <div>
-                <div>{new Date(scan.started_at).toLocaleString()}</div>
+        <div className={styles.file_scan_history_item}>
+            <div className={styles.item_left_section}>
+                <div className={styles.item_date}>{formatted_finish_date}</div>
             </div>
 
-            <div>
-                <div>{scan.detection_count} detections</div>
-                <div>{scan.finished_at ? "Completed" : "In Progress"}</div>
+            <div className={styles.item_right_section}>
+                <div className={styles.item_detection_count}>
+                    {scan.detection_count}{" "}
+                    {scan.detection_count === 1 ? "detection" : "detections"}
+                </div>
+
+                <div className={styles.item_status}>{scan.finished_at ? "Completed" : "In Progress"}</div>
             </div>
         </div>
     )
