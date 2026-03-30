f26c73ad77065350b046a68cbac3ad7c366bdf11
Oscar Webster <WebsterO1@cardiff.ac.uk>
2026-03-30 07:32:49 +0100

    Added conditional styling and new icons

--- frontend/src/scans/OrganisationScanPage.jsx
+++ frontend/src/scans/OrganisationScanPage.jsx
@@ -1,7 +1,14 @@
 import { PiFileBold } from "react-icons/pi";
+import { PiTextAaBold } from "react-icons/pi";
+import { PiCardsBold } from "react-icons/pi";
+import { PiCheckCircleBold } from "react-icons/pi";
+
+
+
 
 function OrganisationScanPage({ scan }) {
     return (
+        // To change the colour of a card, apply either 'issues' or 'clean' to the scan-page-card class
         <div className="scan-page-card-container">
             <div className="scan-page-card">
                 <div className="scan-page-card-text">
@@ -13,34 +20,36 @@ function OrganisationScanPage({ scan }) {
                     <PiFileBold size={50}/>
                 </div>
             </div>
-            <div className="scan-page-card">
+            <div className="scan-page-card issues">
                 <div className="scan-page-card-text">
-                    <span className="scan-page-card-subtitle">Total Files Scanned</span>
-                    <span className="scan-page-card-title">{scan.file_count}</span>
+                    <span className="scan-page-card-subtitle">Naming Issues</span>
+                    {/* HARDCODED FOR NOW */}
+                    <span className="scan-page-card-title">7</span>
                     
                 </div>
                 <div>
-                    <PiFileBold size={50}/>
+                    <PiTextAaBold size={50}/>
                 </div>
             </div>
-            <div className="scan-page-card">
+            <div className="scan-page-card clean">
                 <div className="scan-page-card-text">
-                    <span className="scan-page-card-subtitle">Total Files Scanned</span>
-                    <span className="scan-page-card-title">{scan.file_count}</span>
+                    <span className="scan-page-card-subtitle">Duplicate Files</span>
+                    <span className="scan-page-card-title">0</span>
                     
                 </div>
                 <div>
-                    <PiFileBold size={50}/>
+                    <PiCardsBold size={50}/>
                 </div>
             </div>
-            <div className="scan-page-card">
+            {/* need to create function to calculate % */}
+            <div className="scan-page-card clean">
                 <div className="scan-page-card-text">
-                    <span className="scan-page-card-subtitle">Total Files Scanned</span>
-                    <span className="scan-page-card-title">{scan.file_count}</span>
+                    <span className="scan-page-card-subtitle">Clean Files</span>
+                    <span className="scan-page-card-title">87%</span>
                     
                 </div>
                 <div>
-                    <PiFileBold size={50}/>
+                    <PiCheckCircleBold size={50}/>
                 </div>
             </div>
             
