071b89143c86c999bfa55e3d89ae4dafaa1214b4
Szymon Wodkiewicz <wodkiewiczs@cardiff.ac.uk>
2026-04-09 16:27:52 +0100

    implemented repository method for getting thresholds of a provided role

--- backend/app/access_mapping/repository.py
+++ backend/app/access_mapping/repository.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from app.authentication.models import User
-from app.roles.models import UserRole, Role
+from app.roles.models import UserRole, Role, RolePermission, SensitivitySubcategory
 from app.ingestion.models import UserFiles
 
 
@@ -18,5 +18,21 @@ def get_file_employees_with_access(db: Session, file_id: int):
         .outerjoin(UserRole, UserRole.user_id == User.user_id)
         .outerjoin(Role, Role.role_id == UserRole.role_id)
         .filter(UserFiles.file_id == file_id)
+        .all()
+    )
+
+
+# Method to get the thresholds of a provided role
+def get_role_thresholds(db: Session, role_id: int):
+    return (
+        db.query(
+            SensitivitySubcategory.name.label("subcategory"),
+            RolePermission.threshold
+        )
+        .join(
+            SensitivitySubcategory,
+            SensitivitySubcategory.sensitivity_subcategory_id == RolePermission.sensitivity_subcategory_id
+        )
+        .filter(RolePermission.role_id == role_id)
         .all()
     )
