7ae8dc8da7890adc8961c2d6106a87afb439d6a3
Daiyan Khan <khand6@cardiff.ac.uk>
2026-04-09 15:19:23 +0100

    Have added coverage reports for the frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,12 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     setupFiles: './src/setupTests.js',
+    coverage: {
+      provider: 'istanbul',
+      reportOnFailure: true,
+      exclude: ['src/assets', 'src/App.{css,jsx}', 'src/Test/', 'src/Errors/', '**.css'],
+      reporter: ['cobertura', 'html']
+    }
   },
   server: {
     watch: {
