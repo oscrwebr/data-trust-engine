4cb62c6abfa509c7a548062558f323fa666e8751
Szymon Wodkiewicz <wodkiewiczs@cardiff.ac.uk>
2026-04-10 14:43:05 +0100

    improved styling and layout of employee cards

--- frontend/src/FileOverview/EmployeeAccessItem.jsx
+++ frontend/src/FileOverview/EmployeeAccessItem.jsx
@@ -6,18 +6,21 @@ import { FaExclamationCircle } from "react-icons/fa";
 function EmployeeAccessItem({ employee }) {
     return (
         <div className={styles.employee_access_item}>
-            <div className={styles.employee_top_row}>
-                <div className={styles.employee_name}>{employee.name}</div>
-                <div className={styles.employee_email}>{employee.email}</div>
-            </div>
-
             <div className={styles.employee_access_status_icon}>
                 {employee.access_allowed ? (<FaCheckCircle className={styles.allowed_icon}/>) : (<FaExclamationCircle className={styles.denied_icon}/>)}
             </div>
 
-            <div className={styles.employee_roles}>
-                <div className={styles.roles_label}>Roles:</div>{" "}
-                {employee.roles.length > 0 ? employee.roles.join(", ") : "No roles assigned"}
+            <div className={styles.employee_content}>
+                <div className={styles.employee_information}>
+                    <div className={styles.employee_name}>{employee.name}</div>
+                    <div className={styles.employee_email}>{employee.email}</div>
+                </div>
+
+                <div className={styles.employee_roles}>
+                    <div>
+                        {employee.roles.length > 0 ? employee.roles.join(", ") : "No roles assigned"}
+                    </div>
+                </div>
             </div>
         </div>
     );
