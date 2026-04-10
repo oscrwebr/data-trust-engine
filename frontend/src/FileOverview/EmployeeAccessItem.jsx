ec057c9fa49ae2a0a8e905dd537b1e8a8b6c6f09
Szymon Wodkiewicz <wodkiewiczs@cardiff.ac.uk>
2026-04-10 14:18:10 +0100

    implemented access status icon in EmployeeAccessItem component which describes whether employee should have access or not

--- frontend/src/FileOverview/EmployeeAccessItem.jsx
+++ frontend/src/FileOverview/EmployeeAccessItem.jsx
@@ -1,11 +1,18 @@
 import styles from "./EmployeeAccessItem.module.css";
 
+import { FaCheckCircle } from "react-icons/fa";
+import { FaExclamationCircle } from "react-icons/fa";
+
 function EmployeeAccessItem({ employee }) {
     return (
         <div className={styles.employee_access_item}>
-            <div className={styles.employee_info}>
+            <div className={styles.employee_top_row}>
                 <div className={styles.employee_name}>{employee.name}</div>
                 <div className={styles.employee_email}>{employee.email}</div>
+            </div>
+
+            <div className={styles.employee_access_status_icon}>
+                {employee.access_allowed ? (<FaCheckCircle className={styles.allowed_icon}/>) : (<FaExclamationCircle className={styles.denied_icon}/>)}
             </div>
 
             <div className={styles.employee_roles}>
