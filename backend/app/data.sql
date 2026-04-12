c75b5e2fc8471bd81df91223d69b9f9eba8c0f0d
Szymon Wodkiewicz <wodkiewiczs@cardiff.ac.uk>
2026-04-12 14:46:27 +0100

    fixed broken data.sql code

--- backend/app/data.sql
+++ backend/app/data.sql
@@ -131,7 +131,7 @@ INSERT INTO `role_permission` (`role_permission_id`, `role_id`, `sensitivity_sub
 (5, 1, 10, 0),
 (6, 1, 11, 0),
 (7, 2, 1, 0),
-(8, 2, 2, 0,
+(8, 2, 2, 0),
 (9, 2, 3, 0),
 (10, 2, 4, 0),
 (11, 2, 5, 0),
