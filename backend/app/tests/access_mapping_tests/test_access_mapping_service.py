54a5c56c3ee0cb767aadc9970ebf6217a5f92db0
Szymon Wodkiewicz <wodkiewiczs@cardiff.ac.uk>
2026-04-13 12:59:25 +0100

    implemented test method for evaluate_employee_access, changed how service is imported in tests, added more comments for clarity

--- backend/app/tests/access_mapping_tests/test_access_mapping_service.py
+++ backend/app/tests/access_mapping_tests/test_access_mapping_service.py
@@ -1,4 +1,4 @@
-from app.access_mapping.service import *
+import app.access_mapping.service as service
 from types import SimpleNamespace
 from unittest.mock import Mock
 
@@ -22,7 +22,7 @@ def test_build_employees_from_record_groups_roles_for_same_employee():
         )
     ]
 
-    employees_dict = build_employees_from_records(fetched_employees_records=test_employee_records)
+    employees_dict = service.build_employees_from_records(fetched_employees_records=test_employee_records)
 
     # Ensure only one employee item exists with both roles in roles list, and with all correct details
     assert len(employees_dict) == 1
@@ -53,7 +53,7 @@ def test_build_employees_from_records_does_not_duplicate_same_role():
         )
     ]
 
-    employees_dict = build_employees_from_records(test_employee_records)
+    employees_dict = service.build_employees_from_records(test_employee_records)
 
     # Ensure employee only has one role (role is not duplicated)
     assert employees_dict[1]["roles"] == ["PII Role"]
@@ -71,7 +71,7 @@ def test_build_employees_from_records_handles_employee_with_no_role_name():
         )
     ]
 
-    employees = build_employees_from_records(test_employee_records)
+    employees = service.build_employees_from_records(test_employee_records)
 
     # Ensure only one item in employees dictionary, ensure roles list is empty
     assert len(employees) == 1
@@ -86,9 +86,10 @@ def test_build_effective_thresholds_returns_thresholds_for_single_role(monkeypat
             SimpleNamespace(subcategory="EMAIL", threshold=2)
         ]
 
-    monkeypatch.setattr(repository, "get_role_permissions", mock_get_role_permissions)
+    # Mock the get_role_permission repository method
+    monkeypatch.setattr(service.repository, "get_role_permissions", mock_get_role_permissions)
 
-    thresholds = build_effective_thresholds(db=Mock(), role_ids=[1])
+    thresholds = service.build_effective_thresholds(db=Mock(), role_ids=[1])
 
     # Ensure the correct thresholds are returned
     assert thresholds == {
@@ -112,9 +113,10 @@ def test_build_effective_thresholds_keeps_most_permissive_threshold_across_roles
             ]
         return []
 
-    monkeypatch.setattr(repository, "get_role_permissions", mock_get_role_permissions)
+    # Mock the get_role_permission repository method
+    monkeypatch.setattr(service.repository, "get_role_permissions", mock_get_role_permissions)
 
-    thresholds = build_effective_thresholds(db=Mock(), role_ids=[1, 2])
+    thresholds = service.build_effective_thresholds(db=Mock(), role_ids=[1, 2])
 
     # Ensure only the MOST PERMISSIVE thresholds are returned (name threshold: 5 | email threshold: 3)
     assert thresholds == {
@@ -138,7 +140,7 @@ def test_get_failed_detections_returns_only_detections_exceeding_thresholds():
         "PHONE": 3
     }
 
-    failed_detections = get_failed_detections(latest_scan_results, effective_thresholds)
+    failed_detections = service.get_failed_detections(latest_scan_results, effective_thresholds)
 
     # Ensure failed detections are those ONLY where count is greater than threshold
     assert failed_detections == [
@@ -161,7 +163,7 @@ def test_get_failed_detections_does_not_fail_when_detection_equals_threshold():
         "NAME": 5
     }
 
-    failed_detections = get_failed_detections(latest_scan_results, effective_thresholds)
+    failed_detections = service.get_failed_detections(latest_scan_results, effective_thresholds)
 
     # Ensure theres no failed detections, because the detection count equals to the threshold, which is allowed
     assert failed_detections == []
@@ -176,7 +178,77 @@ def test_get_failed_detections_does_not_fail_when_subcategory_has_no_threshold()
     # Mock effective thresholds dictionary
     effective_thresholds = {}
 
-    failed_detections = get_failed_detections(latest_scan_results, effective_thresholds)
+    failed_detections = service.get_failed_detections(latest_scan_results, effective_thresholds)
 
     # Ensure theres no failed detections, because no threshold exists for NAME subcategory, therefore unlimited amount of detections allowed
     assert failed_detections == []
+
+
+def test_evaluate_employee_access_denies_employee_with_no_roles(monkeypatch):
+    # Mock employee
+    employee = {
+        "user_id": 1,
+        "name": "Test Account",
+        "email": "testaccount@test.com",
+        "roles": [],
+        "access_allowed": True,
+        "failed_detections": []
+    }
+
+    # Mock repository call to return roles for this user
+    monkeypatch.setattr(service.repository, "get_user_role_ids", lambda db, user_id: [])
+
+    service.evaluate_employee_access(
+        db=Mock(),
+        employee=employee,
+        latest_scan_results=[]
+    )
+
+    # Ensure employee is not allowed access, and ensure reason is because they have no roles assigned
+    assert employee["access_allowed"] is False
+    assert employee["failed_detections"] == [
+        {
+            "subcategory": "NO_ROLES_ASSIGNED",
+            "count": None,
+            "threshold": None
+        }
+    ]
+
+
+def test_evaluate_employee_access_allows_employee_when_no_failed_detections(monkeypatch):
+    # Mock employee
+    employee = {
+        "user_id": 1,
+        "name": "Test Account",
+        "email": "testaccount@test.com",
+        "roles": ["PII Role"],
+        "access_allowed": True,
+        "failed_detections": []
+    }
+
+    # Mock repository call to return a role for this user
+    monkeypatch.setattr(service.repository, "get_user_role_ids", lambda db, user_id: [1])
+
+    # Mock the build threshold logic (simulate that employee is allowed up to 10 name detections)
+    monkeypatch.setattr(
+        service,
+        "build_effective_thresholds",
+        lambda db, role_ids: {"NAME": 10}
+    )
+
+    # Mock the get failed detections logic (simulate that no detections violate employee's thresholds)
+    monkeypatch.setattr(
+        service,
+        "get_failed_detections",
+        lambda latest_scan_results, effective_thresholds: []
+    )
+
+    service.evaluate_employee_access(
+        db=Mock(),
+        employee=employee,
+        latest_scan_results=[{"subcategory": "NAME", "count": 5}]
+    )
+
+    # Ensure access allowed is true and there are no failed detections
+    assert employee["access_allowed"] is True
+    assert employee["failed_detections"] == []
