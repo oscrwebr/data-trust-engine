e4beaa7a3370057e67dfc89929d80d0bda4b89dc
Daiyan Khan <khand6@cardiff.ac.uk>
2026-04-13 15:01:28 +0100

    Updated styling for dropdown item without a link

--- frontend/src/components/navbar/DropdownItemNoLink.jsx
+++ frontend/src/components/navbar/DropdownItemNoLink.jsx
@@ -8,7 +8,7 @@ function DropdownItemNoLink({ url, text, icon, onClick, value }) {
 
     return (
         <li>
-            <button  className={`${styles.dropdown_item_link} ${isActive ? styles.active : ""}`} onClick={onClick}>
+            <button className={`${styles.dropdown_item_link} ${isActive ? styles.active : ""}`} onClick={onClick}>
                 <i className={icon}/>
                 <span>{text}</span>
                 {value != null && <Badge value={value} style={{ marginLeft: 'auto' }}/>}
