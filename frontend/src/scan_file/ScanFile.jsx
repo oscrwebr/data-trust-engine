9e45154603170435dcff6d74eb2e4411fc263481
Oscar Webster <WebsterO1@cardiff.ac.uk>
2026-04-16 23:51:34 +0100

    Frontend fetches data from backend route

--- frontend/src/scan_file/ScanFile.jsx
+++ frontend/src/scan_file/ScanFile.jsx
@@ -1,6 +1,8 @@
 import { use, useEffect, useState } from "react";
 import { Divider } from 'primereact/divider';
 import "../scans/scans.css";
+import api from "../api/axiosConfig";
+import { Link, useParams } from "react-router-dom";
 
 
 function ScanFile({ scan_file }) {
@@ -8,12 +10,20 @@ function ScanFile({ scan_file }) {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [scanFile, setScanFile] = useState(null);
-    const scanFileId = useParams()
+    const { scanFileId } = useParams();
 
-//     useEffect(() => {
-//         api.get(`/scanning/`)
-
-// }, [scanFileId])
+    useEffect(() => {
+        api.get(`/scanning/get_scan_file_by_id/${scanFileId}`)
+        .then(response => {
+            setLoading(false);
+            setScanFile(response.data);
+        })
+        .catch(error => {
+            console.error("Error fetching scanned file:", error);
+            setError(error);
+            setLoading(false);
+        })
+    }, [scanFileId])
 
     return (
         <div>
@@ -23,16 +33,14 @@ function ScanFile({ scan_file }) {
                     <p className="scan-loading">Error loading scanned file.</p>
                 ) : scanFile === null ? (
                     <p className="scan-loading">No scanned file found.</p>
-                ) : ScanTypePage ? ( 
+                ) : (
                     <>
                         <div className="scan-header">
                             <h1 className="scan-heading">
-                                Scan File {scan_file.scan_file_id} Results
+                                Scan File {scanFile.scan_file_id} Results
                             </h1>
                         </div>
                     </>
-                ) : (
-                    <p className="scan-loading">Error fetching scan file.</p>
                 )}
         </div>
         
