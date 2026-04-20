3308ba9bae9a26a3eebe3178fb004f11f330f3f3
Daiyan Khan <khand6@cardiff.ac.uk>
2026-04-21 00:31:24 +0100

    Have updated bash script for production server to include compose file and add a daemon.json file that will prevent docker compose networks interfering with azure IP addresses and SSH

--- terraform/project.sh
+++ terraform/project.sh
@@ -33,7 +33,131 @@ echo \
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# Add vagrant user to docker group - means that 'sudo' command isn't required each time!
-# usermod -aG docker vagrant
+# Login to docker for access to personal registry
+echo "dckr_pat_kMcmoi1oTECeoSm2qG7V0ESi_Eo" | sudo docker login -u dk04 --password-stdin
 
 # Above was found from HashiCorp at: https://developer.hashicorp.com/vagrant/tutorials/get-started/provision
+cd /
+echo "Creating the app directory and adding the compose.yaml file"
+mkdir app
+cd app
+touch compose.yaml
+touch .env
+cat << `EOF` >> compose.yaml
+services:
+  # Setting up redis for dev and prod environments
+  redis:
+    image: redis:8.6.2-alpine
+
+  # Setting up the prod env -- TO BE PROPERLY CONFIGURED WHEN SETTING UP CLOUD DEPLOYMENT
+  database-prod:
+    image: mariadb:11.8-noble
+    environment:
+      - MARIADB_ROOT_PASSWORD=\${DB_PASSWORD}
+      - MARIADB_DATABASE=\${DB_NAME}
+    volumes:
+      - dte-prod-db-volume:/var/lib/mysql
+    healthcheck:
+      test: ["CMD", "healthcheck.sh", "--connect", "--innodb_initialized"] # https://mariadb.com/docs/server/server-management/automated-mariadb-deployment-and-administration/docker-and-mariadb/using-healthcheck-sh
+      start_period: 40s
+      interval: 10s
+      timeout: 5s
+      retries: 5
+
+  alembic_migrations-prod:
+    image: dk04/data-trust-engine-registry:backend-latest
+    environment:
+      - DB_HOST=database-prod
+      - DB_USERNAME=\${DB_USERNAME}
+      - DB_PASSWORD=\${DB_PASSWORD}
+      - DB_NAME=\${DB_NAME}
+      - DB_TEST_NAME=\${DB_TEST_NAME}
+    command: ["python", "-m", "alembic", "-c", "alembic.ini", "upgrade", "head"]
+    depends_on:
+      database-prod:
+        condition: service_healthy
+
+  backend-prod:
+    image: dk04/data-trust-engine-registry:backend-latest
+    pull_policy: never
+    environment:
+      - CLIENT_ID=\${CLIENT_ID}
+      - CLIENT_SECRET=\${CLIENT_SECRET}
+      - AUTHORITY=\${AUTHORITY}
+      - SCOPES=\${SCOPES}
+      - DB_HOST=database-prod
+      - DB_USERNAME=\${DB_USERNAME}
+      - DB_PASSWORD=\${DB_PASSWORD}
+      - DB_NAME=\${DB_NAME}
+      - DB_TEST_NAME=\${DB_TEST_NAME}
+      - ACCESS_TOKEN_SECRET=\${ACCESS_TOKEN_SECRET}
+      - ALGORITHM=\${ALGORITHM}
+      - MAIL_PASSWORD=\${MAIL_PASSWORD}
+      - ZEROBOUNCE_API_KEY=\${ZEROBOUNCE_API_KEY}
+      - FERNET_KEY=\${FERNET_KEY}
+      - FRONTEND_HOST=http://localhost #  THESE MUST ALWAYS BE TO WHATEVER THE USER'S CLIENT SHOULD BE! FOR DEV, THAT IS LOCALHOST
+      - REDIRECT_URI=http://localhost/api
+      - REDIS_HOST=redis
+    depends_on:
+      alembic_migrations-prod:
+        condition: service_completed_successfully
+
+  frontend-prod:
+    image: dk04/data-trust-engine-registry:frontend-latest
+    ports:
+      - 80:80
+    depends_on:
+      - backend-prod
+
+  # Setting up celery for prod env
+  celery-prod:
+    image: dk04/data-trust-engine-registry:backend-latest
+    pull_policy: never
+    environment:
+      - CLIENT_ID=\${CLIENT_ID}
+      - CLIENT_SECRET=\${CLIENT_SECRET}
+      - AUTHORITY=\${AUTHORITY}
+      - SCOPES=\${SCOPES}
+      - DB_HOST=database-prod
+      - DB_USERNAME=\${DB_USERNAME}
+      - DB_PASSWORD=\${DB_PASSWORD}
+      - DB_NAME=\${DB_NAME}
+      - DB_TEST_NAME=\${DB_TEST_NAME}
+      - ACCESS_TOKEN_SECRET=\${ACCESS_TOKEN_SECRET}
+      - ALGORITHM=\${ALGORITHM}
+      - MAIL_PASSWORD=\${MAIL_PASSWORD}
+      - ZEROBOUNCE_API_KEY=\${ZEROBOUNCE_API_KEY}
+      - FERNET_KEY=\${FERNET_KEY}
+      - FRONTEND_HOST=http://localhost #  THESE MUST ALWAYS BE TO WHATEVER THE USER'S CLIENT SHOULD BE! FOR DEV, THAT IS LOCALHOST
+      - REDIRECT_URI=http://localhost/api
+      - REDIS_HOST=redis
+    command: ["celery","-A","app.core.celery_worker.celery", "worker", "--loglevel=info", "--concurrency=1"]
+    depends_on:
+      - redis
+      - backend-prod
+
+volumes:
+  dte-prod-db-volume:
+`EOF`
+
+sudo docker compose pull
+
+echo "Ensuring that docker network doesn't interfere with SSH"
+cd /etc/docker
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "default-address-pools": [
+    {
+      "base": "172.20.0.0/16",
+      "size": 24
+    }
+  ],
+  "bip": "172.19.0.1/24",
+  "mtu": 1400,
+  "labels": ["vm_ip=$VM_IP"]
+}
+EOF
+sudo systemctl restart docker
+cd /app
+
+
