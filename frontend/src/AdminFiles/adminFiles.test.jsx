b8d24595b56949dba9dd850dac846800d059c124
Samuel Carter <carters11@cardiff.ac.uk>
2026-04-22 13:22:45 +0100

    updated a upercase to lowercase

--- frontend/src/AdminFiles/adminFiles.test.jsx
+++ frontend/src/AdminFiles/adminFiles.test.jsx
@@ -8,7 +8,7 @@ import {
   within,
 } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
-import AdminFiles from "./AdminFiles";
+import AdminFiles from "./adminFiles";
 
 // ✅ mock axios api
 vi.mock("../api/axiosConfig", () => ({
