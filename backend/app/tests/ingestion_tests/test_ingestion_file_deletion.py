973fbfb27b9c6438b565d1dd5332120f0aec22b1
Daiyan Khan <khand6@cardiff.ac.uk>
2026-04-22 12:35:18 +0100

    Added final tests for deleting files feature

--- backend/app/tests/ingestion_tests/test_ingestion_file_deletion.py
+++ backend/app/tests/ingestion_tests/test_ingestion_file_deletion.py
@@ -8,7 +8,7 @@ from app.core.security import application
 from app.authentication.models import User
 from app.authentication.service import create_access_refresh
 from app.authentication.repository import create_refresh_family
-from app.ingestion.models import IngestionFile
+from app.ingestion.models import IngestionFile, UserFiles
 from app.ingestion.service import GRAPH_PATCH_NAME
 from app.core.security import encrypt_refresh, decrypt_refresh
 from app.workspaces.models import Workspace
@@ -128,6 +128,61 @@ def test_400_if_file_does_not_exist(db, client):
     # ASSERTIONS
     assert res.status_code == 400
 
+# Test that when microsoft returns a non-200 status, 400 is returned to client
+def test_400_when_ms_return_non_2xx(db, client, requests_mock):
+    # create data for the two user that is being tested (adding permissions for both users)
+    oid = "000000-7sdf77-88asdf8-9sdiy99"
+    insert_statement = insert(User).values(firstname="John", surname="Smith", username="johnSmith1@hotmail.com", email="JohnSmith1@hotmail.com", refresh=encrypt_refresh("ms-refresh-token"), oid=oid, role="admin").returning(User)
+    user = db.execute(insert_statement).scalar_one()
+
+    oid = "000000-7sdf77-88asdf8-234sldfkj"
+    insert_statement = insert(User).values(firstname="Mark", surname="Grayson", username="invincible@hotmail.com", email="Invincible@hotmail.com", refresh=encrypt_refresh("ms-refresh-token-2"), oid=oid, role="employee", driveId="dummy-drive-id-to-be-deleted").returning(User)
+    user_2 = db.execute(insert_statement).scalar_one()
+
+    # Creating tokens for admin
+    refresh_family = create_refresh_family(db)
+    access, refresh, _ = create_access_refresh(db, data={"userId": user.user_id, "role": "admin"}, refresh_family_id=refresh_family.refresh_family_id)
+
+    # Creating the workspace for the admin, but not the user
+    image = create_test_image()
+    workspace_insert = insert(Workspace).values(name="Test Workspace", image=image)
+    workspace_insert = db.execute(workspace_insert)
+
+    # Adding admin and employee to the workspace
+    add_user_workspace(db, workspace_insert.inserted_primary_key[0], user.user_id)
+    add_user_workspace(db, workspace_insert.inserted_primary_key[0], user_2.user_id)
+
+    # Creating the dummy ingestion file
+    file_insert_statement = insert(IngestionFile).values(
+        graph_id="graph-1234",
+        name="test-graph",
+        extension="pdf",
+        hash="dummy-hash",
+        hash_type="sha256",
+        last_modified=(datetime.now(timezone.utc) - timedelta(days=7)),
+        web_url="original-web-url",
+        drive_id="dummy-drive-id-to-be-deleted"
+        ).returning(IngestionFile)
+    file = db.execute(file_insert_statement).scalar_one()
+
+    # Adding dependency override for application
+    app.dependency_overrides[application] = lambda: FakeMsal()
+
+    # Mock request
+    delete_ms_response = requests_mock.delete(
+        url=GRAPH_PATCH_NAME.format(graph_id="graph-1234"),
+        status_code=400
+    )
+
+    # Making the call to the endpoint, expecting 204
+    res = client.delete(
+        url = "/ingestion/delete-file?graph_id=graph-1234",
+        headers={"Authorization": f"Bearer {access}"}
+    )
+
+    # ASSERTIONS
+    assert res.status_code == 400
+
 # Test that when microsoft returns 204, user is returned a 200 status
 def test_200_when_ms_return_204(db, client, requests_mock):
     # create data for the two user that is being tested (adding permissions for both users)
@@ -184,4 +239,78 @@ def test_200_when_ms_return_204(db, client, requests_mock):
     assert res.status_code == 200
     assert delete_ms_response.request_history[0]._request.url.split("/")[-1] == 'graph-1234'
     assert delete_ms_response.request_history[0]._request.headers["Authorization"] == "Bearer fake-ms-access-token"
-    print(delete_ms_response.request_history[0]._request.headers.__dict__)
+
+# Test that user_files and ingestion_files are deleted when request to MS is successful
+def test_ingestion_and_user_files_deleted_on_ms_success(db, client, requests_mock):
+    # create data for the two user that is being tested (adding permissions for both users)
+    oid = "000000-7sdf77-88asdf8-9sdiy99"
+    insert_statement = insert(User).values(firstname="John", surname="Smith", username="johnSmith1@hotmail.com", email="JohnSmith1@hotmail.com", refresh=encrypt_refresh("ms-refresh-token"), oid=oid, role="admin").returning(User)
+    user = db.execute(insert_statement).scalar_one()
+
+    oid = "000000-7sdf77-88asdf8-234sldfkj"
+    insert_statement = insert(User).values(firstname="Mark", surname="Grayson", username="invincible@hotmail.com", email="Invincible@hotmail.com", refresh=encrypt_refresh("ms-refresh-token-2"), oid=oid, role="employee", driveId="dummy-drive-id-to-be-deleted").returning(User)
+    user_2 = db.execute(insert_statement).scalar_one()
+
+    # Creating tokens for admin
+    refresh_family = create_refresh_family(db)
+    access, refresh, _ = create_access_refresh(db, data={"userId": user.user_id, "role": "admin"}, refresh_family_id=refresh_family.refresh_family_id)
+
+    # Creating the workspace for the admin, but not the user
+    image = create_test_image()
+    workspace_insert = insert(Workspace).values(name="Test Workspace", image=image)
+    workspace_insert = db.execute(workspace_insert)
+
+    # Adding admin and employee to the workspace
+    add_user_workspace(db, workspace_insert.inserted_primary_key[0], user.user_id)
+    add_user_workspace(db, workspace_insert.inserted_primary_key[0], user_2.user_id)
+
+    # Creating the dummy ingestion file
+    file_insert_statement = insert(IngestionFile).values(
+        graph_id="graph-1234",
+        name="test-graph",
+        extension="pdf",
+        hash="dummy-hash",
+        hash_type="sha256",
+        last_modified=(datetime.now(timezone.utc) - timedelta(days=7)),
+        web_url="original-web-url",
+        drive_id="dummy-drive-id-to-be-deleted"
+        ).returning(IngestionFile)
+    file = db.execute(file_insert_statement).scalar_one()
+
+    # Creating dummy user_files data linked to employee and dumm ingestion_file
+    user_files_insert_statement = insert(UserFiles).values(file_id=file.ingestion_file_id, user_id=user_2.user_id).returning(UserFiles)
+    user_file = db.execute(user_files_insert_statement).scalar_one()
+
+    # Adding dependency override for application
+    app.dependency_overrides[application] = lambda: FakeMsal()
+
+    # Mock request
+    delete_ms_response = requests_mock.delete(
+        url=GRAPH_PATCH_NAME.format(graph_id="graph-1234"),
+        status_code=204
+    )
+
+    # INITIAL_ASSERTIONS
+    # make sure that the dummy data exists in the database
+    ingestion_select = select(IngestionFile).where(IngestionFile.graph_id == file.graph_id)
+    pre_request_ingestion = db.execute(ingestion_select).fetchall()
+    user_files_select = select(UserFiles).where(UserFiles.file_id == user_file.file_id)
+    pre_request_user_file = db.execute(user_files_select).fetchall()
+
+    assert pre_request_ingestion
+    assert pre_request_user_file
+  
+
+    # Making the call to the endpoint, expecting 204
+    res = client.delete(
+        url = "/ingestion/delete-file?graph_id=graph-1234",
+        headers={"Authorization": f"Bearer {access}"}
+    )
+
+    post_request_ingestion = db.execute(ingestion_select).fetchall()
+    post_request_user_file = db.execute(user_files_select).fetchall()
+
+    # ASSERTIONS
+    assert res.status_code == 200
+    assert not post_request_ingestion
+    assert not post_request_user_file
