5e21fbe12d3a00a06a6cba406da3cc3977912803
Daiyan Khan <khand6@cardiff.ac.uk>
2026-04-22 17:17:56 +0100

    Have added logic to ingestion repository to handle the population of the unknown_user_files and unknown_user_folders tables

--- backend/app/ingestion/repository.py
+++ backend/app/ingestion/repository.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import insert, update, delete
-from .models import Folder, IngestionFile, UserFolders, UserFiles
+from .models import Folder, IngestionFile, UserFolders, UserFiles, UnknownUserFolders, UnknownUserFiles
 from datetime import datetime
 
 
@@ -47,13 +47,35 @@ def insert_user_files(user_files: list, db: Session):
         status["status"] = 403
     return status
 
+def insert_unknown_user_folders(user_folders: list, db: Session):
+    status = {
+        "status": 200
+    }
+    try:
+        db.execute(insert(UnknownUserFolders), user_folders)
+        db.commit()
+    except Exception as e:
+        status["description"] = f"An error ocurred: {type(e).__name__} - {e}"
+        status["status"] = 403
+    return status
+
+def insert_unknown_user_files(user_files: list, db: Session):
+    status = {
+        "status": 200
+    }
+    try:
+        db.execute(insert(UnknownUserFiles), user_files)
+        db.commit()
+    except Exception as e:
+        status["description"] = f"An error ocurred: {type(e).__name__} - {e}"
+        status["status"] = 403
+    return status
+
 def get_drive_id_by_graph_id(graph_id: str, db: Session):
     return db.query(IngestionFile.drive_id).where(IngestionFile.graph_id == graph_id).first()
 
-
 def get_ingestion_file_by_graph_id(graph_id: str, db: Session):
     return db.query(IngestionFile).filter(IngestionFile.graph_id == graph_id).first()
-
 
 def create_ingestion_file(db: Session, graph_id: str, name: str, extension: str, last_modified: datetime, web_url: str, drive_id: str,hash: str | None = None,hash_type: str | None = None,last_scanned: datetime | None = None, parent_graph_id: str | None = None):
     ingestion_file = IngestionFile(
@@ -74,7 +96,6 @@ def create_ingestion_file(db: Session, graph_id: str, name: str, extension: str,
     db.refresh(ingestion_file)
 
     return ingestion_file
-
 
 def get_all_files(db: Session):
     return db.query(IngestionFile).all()
